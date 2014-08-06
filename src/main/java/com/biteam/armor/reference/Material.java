04fa9a877afa49e3c4cd2325ebecc3c73e991493
Pa-Paolo <paolobono98@gmail.com>
2014-08-06 23:23:58 +0200

    Add atomic and Basic Material

--- src/main/java/com/biteam/armor/reference/Material.java
+++ src/main/java/com/biteam/armor/reference/Material.java
@@ -9,10 +9,14 @@ public class Material
     public static final class Tools
     {
         public static final Item.ToolMaterial EPIC_MATTER = EnumHelper.addToolMaterial("EPIC_MATTER", 3, 0, 30f, 96f, 0);
+        public static final Item.ToolMaterial ATOMIC_MATTER = EnumHelper.addToolMaterial("ATOMIC_MATTER", 3, 0, 30f, 96f, 0);
+        public static final Item.ToolMaterial BASIC_MATTER = EnumHelper.addToolMaterial("BASIC_MATTER", 3, 0, 30f, 96f, 0);
     }
 
     public static final class Armor
     {
         public static final ItemArmor.ArmorMaterial EPIC_MATTER = EnumHelper.addArmorMaterial("EPIC_MATTER", 0, new int[]{3, 8, 6, 3}, 0);
+        public static final ItemArmor.ArmorMaterial ATOMIC_MATTER = EnumHelper.addArmorMaterial("ATOMIC_MATTER", 0, new int[]{3, 8, 6, 3}, 0);
+        public static final ItemArmor.ArmorMaterial BASIC_MATTER = EnumHelper.addArmorMaterial("BASIC_MATTER", 0, new int[]{3, 8, 6, 3}, 0);
     }
 }
