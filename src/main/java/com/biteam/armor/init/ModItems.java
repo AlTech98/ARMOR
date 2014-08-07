63c32505f15a744a4d1d628cb55229d828cc9de5
AlTech98 <altech123159@gmail.com>
2014-08-07 16:23:51 +0200

    Add Epic Armor

--- src/main/java/com/biteam/armor/init/ModItems.java
+++ src/main/java/com/biteam/armor/init/ModItems.java
@@ -4,10 +4,7 @@ package com.biteam.armor.init;
 import com.biteam.armor.item.*;
 import com.biteam.armor.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemPickaxe;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.*;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems
@@ -22,6 +19,10 @@ public class ModItems
     public static final ItemARMOR atomicCrystal = new ItemAtomicCrystal();
     public static final ItemARMOR epicCrystal = new ItemEpicCrystal();
     public static final ItemSword atomicSword = new ItemAtomicSword();
+    public static final ItemArmor epicHelmet = new ItemEpicArmor("epicHelmet", 0, 0);
+    public static final ItemArmor epicChestplate = new ItemEpicArmor("epicChestplate", 0, 1);
+    public static final ItemArmor epicLegs = new ItemEpicArmor("epicLegs", 0, 2);
+    public static final ItemArmor epicBoots = new ItemEpicArmor("epicBoots", 0, 3);
 
 
     public static void init()
@@ -36,5 +37,9 @@ public class ModItems
         GameRegistry.registerItem(atomicCrystal, "atomicCrystal");
         GameRegistry.registerItem(epicCrystal, "epicCrystal");
         GameRegistry.registerItem(atomicSword, "atomicSword");
+        GameRegistry.registerItem(epicHelmet, "epicHelmet");
+        GameRegistry.registerItem(epicChestplate, "epicChestplate");
+        GameRegistry.registerItem(epicLegs, "epicLegs");
+        GameRegistry.registerItem(epicBoots, "epicBoots");
     }
 }
---
+++ src/main/java/com/biteam/armor/item/ItemEpicArmor.java
@@ -0,0 +1,68 @@
+package com.biteam.armor.item;
+
+import com.biteam.armor.creativetab.CreativeTabARMOR;
+import com.biteam.armor.init.ModItems;
+import com.biteam.armor.reference.Material;
+import com.biteam.armor.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+
+public class ItemEpicArmor extends ItemArmor
+{
+    public ItemEpicArmor(String name, int id, int placement)
+    {
+        super(Material.Armor.EPIC_MATTER, id, placement);
+        this.setUnlocalizedName(name);
+        this.setCreativeTab(CreativeTabARMOR.ARMOR_TAB);
+
+        if (placement == 0) this.setTextureName(Reference.MOD_ID + ":epicHelmet");
+        if (placement == 1) this.setTextureName(Reference.MOD_ID + ":epicChestplate");
+        if (placement == 2) this.setTextureName(Reference.MOD_ID + ":epicLegs");
+        if (placement == 3) this.setTextureName(Reference.MOD_ID + ":epicBoots");
+    }
+
+    public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
+    {
+        if(stack.getItem() == ModItems.epicHelmet || stack.getItem() == ModItems.epicChestplate || stack.getItem() == ModItems.epicBoots)
+        {
+            return Reference.MOD_ID + ":models/armor/epicArmor_1.png";
+        }
+        if(stack.getItem() == ModItems.epicLegs)
+        {
+            return Reference.MOD_ID + ":models/armor/epicArmor_2.png";
+        }
+        else return null;
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack)
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister)
+    {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+}
