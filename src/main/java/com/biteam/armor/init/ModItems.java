c764dc413d9e36efcaefba66145b3bd0c465c5d0
Crasher97 <davide.freggiaro97@gmail.com>
2014-08-07 11:47:08 +0200

    Add test bloodAxe

--- src/main/java/com/biteam/armor/init/ModItems.java
+++ src/main/java/com/biteam/armor/init/ModItems.java
@@ -3,11 +3,13 @@ package com.biteam.armor.init;
 
 import com.biteam.armor.item.ItemARMOR;
 import com.biteam.armor.item.ItemAtomicPickaxe;
+import com.biteam.armor.item.ItemBloodAxe;
 import com.biteam.armor.item.ItemEpicSword;
 import com.biteam.armor.reference.Material;
 import com.biteam.armor.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSword;
 
@@ -16,11 +18,13 @@ public class ModItems
 {
     public static final ItemSword epicSword = new ItemEpicSword();
     public static final ItemPickaxe atomicPickaxe = new ItemAtomicPickaxe();
+    public static final ItemAxe bloodAxe = new ItemBloodAxe();
 
 
     public static void init()
     {
         GameRegistry.registerItem(epicSword, "epicSword");
         GameRegistry.registerItem(atomicPickaxe, "atomicPickaxe");
+        GameRegistry.registerItem(bloodAxe, "bloodAxe");
     }
 }
